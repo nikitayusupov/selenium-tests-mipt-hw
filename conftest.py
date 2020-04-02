354806a68391e0f23039bb09d68cfe6ea1306103
nikitayusupov <nikitayusupov@gmail.com>
2020-04-02 14:27:41 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,7 +5,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 @pytest.fixture
 def browser_is_opened():
-    driver = webdriver.Chrome()
+    chrome_opt = webdriver.ChromeOptions()
+    chrome_opt.add_argument('--headless')
+    chrome_opt.add_argument('--no-sandbox')
+    driver = webdriver.Chrome('./chromedriver', options=chrome_opt)
+    # driver = webdriver.Chrome()
     wait = WebDriverWait(driver, 10)
     yield driver, wait
     driver.close()
